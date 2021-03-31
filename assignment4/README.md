90334cdedd83cc39b70ae5587ee0b7c2016ae240
viraj-rokade <winviraj@gmail.com>
2021-03-30 22:25:01 -0400

    Assignment 4 added

---
+++ assignment4/README.md
@@ -0,0 +1,4 @@
+## Strings and Template Literals
+
+1. Recreate the `description` variable from the last assignment, this time
+using the template literal syntax
---
+++ assignment4/main.js
@@ -0,0 +1,8 @@
+let language;
+const country = "India";
+let population = 1.34;
+const continent = "Asia";
+language = "Hindi";
+
+const description =  `${country} is in ${continent}, and its ${population} Billion population speaks ${language}.`;
+console.log(description);
