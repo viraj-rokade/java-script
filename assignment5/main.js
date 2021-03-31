79d0aa8849a23252d5709bd5f6b00f3604248c2e
viraj-rokade <winviraj@gmail.com>
2021-03-31 13:50:07 -0400

    assignment5 added

---
+++ assignment5/main.js
@@ -0,0 +1,22 @@
+/* Population in billion */
+
+let population = 1.34;
+const country = "India";
+
+if(population > 0.33){
+    console.log(`${country}'s population is above average`);
+}
+else{
+    console.log(`${country}'s population is ${population} Billion is below average.`)
+}
+
+population = 0.13;
+
+if(population > 0.33){
+    console.log(`${country}'s population is above average`);
+}
+else{
+    console.log(`${country}'s population is ${population} Billion is below average.`)
+}
+
+population = 1.34;
