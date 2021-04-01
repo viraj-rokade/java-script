4e844cd4f0a0a096bb8c040ed9fa53d87a0c4085
viraj-rokade <winviraj@gmail.com>
2021-04-01 11:35:38 -0400

    assignment7 added

---
+++ assignment7/main.js
@@ -0,0 +1,9 @@
+const numNeighbours = Number(prompt("How many neighbour countries does your country have?"),);
+
+if (numNeighbours === 1) {
+    console.log('Only 1 border!');
+} else if (numNeighbours > 1) {
+    console.log('More than 1 border');
+} else {
+    console.log('No borders');
+}
