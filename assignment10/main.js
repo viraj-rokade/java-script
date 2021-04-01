e588707272f22008d10cd8ed3887bd2ad5b48271
viraj-rokade <winviraj@gmail.com>
2021-04-01 12:06:29 -0400

    Assignment 9 and 10 added

---
+++ assignment10/main.js
@@ -0,0 +1,14 @@
+let country = "India";
+let language = "Hindi";
+let isIsland = false;
+let population = 1340 ;
+
+population = 130;
+
+population > 33 ? console.log(`${country}'s population is above average`) : console.log(`${country}'s population is below average`);
+
+population = 13;
+
+population > 33 ? console.log(`${country}'s population is above average`) : console.log(`${country}'s population is below average`);
+
+population = 1340;
